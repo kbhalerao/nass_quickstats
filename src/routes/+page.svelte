88dc60da0ee4c15beafe80ce3514f8d83ba323a8
Kaustubh Bhalerao <kaustubh@soildiagnostics.com>
2024-09-27 20:57:48 -0500

    added README

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,7 @@
 	import Browser from '$lib/Browser.svelte';
 </script>
 
-<h1>NASS QuickStats Browser</h1>
+<h1>NASS <i>Quicker</i>Stats Browser</h1>
 <p>You will need your own API key in the .env file under the key PRIVATE_QUICKSTATS_API_KEY</p>
-
 <Browser />
+<p>All data courtey of <a href="https://quickstats.nass.usda.gov/">USDA NASS</a></p>
