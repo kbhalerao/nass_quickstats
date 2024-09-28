637e686dbaa02ae3937d300838e6f77d4fa4f0ef
Kaustubh Bhalerao <kaustubh@soildiagnostics.com>
2024-09-28 14:22:51 -0500

    small update to main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,8 +4,12 @@
 
 <h1>NASS <i>Quicker</i>Stats Browser by AgSci LLC</h1>
 <Browser />
-<p>All data courtey of <a href="https://quickstats.nass.usda.gov/">USDA NASS</a></p>
 <p>
-	Replicate this project. you will need your own API key from NASS in the .env file under the key
+	All data courtey of <a href="https://quickstats.nass.usda.gov/">USDA NASS</a>.
+</p>
+<p>
+	Replicate this project. Find the code for this project on <a
+		href="https://github.com/kbhalerao/nass_quickstats">GitHub</a
+	>. You will need your own API key from NASS in the .env file under the key
 	PRIVATE_QUICKSTATS_API_KEY
 </p>
